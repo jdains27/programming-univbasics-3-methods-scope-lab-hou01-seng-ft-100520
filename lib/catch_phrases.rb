604efe73d8c086e441062c69f00462e6ecda3b7a
Jordan Dains <dains.jordan2@gmail.com>
2020-09-05 19:28:54 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario
-  phrase = ''
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
@@ -7,3 +7,6 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  phrase = ""
